6aae206d3f5a3806a57dd465eabcfd3d921eeedd
Vladislav Mikitich <vmikitich@gmail.com>
2021-07-19 23:30:53 +0300

    fix down migrations

--- scripts/migrations/postgres/20210701013012_reset-account.down.sql
+++ scripts/migrations/postgres/20210701013012_reset-account.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE "like" DROP CONSTRAINT like_post_owner_post_uuid_fkey;
+ALTER TABLE "like" DROP CONSTRAINT like_post_owner_fkey;
 ALTER TABLE "like" ADD FOREIGN KEY (post_owner, post_uuid) REFERENCES post(owner, uuid);
 
 COMMIT;
