216a4cb31b8d9503ef50d57901724669777c1ea4
Vladislav Mikitich <vmikitich@gmail.com>
2021-09-27 22:08:05 +0300

    fix migration

--- scripts/migrations/postgres/20210701013012_reset-account.up.sql
+++ scripts/migrations/postgres/20210701013012_reset-account.up.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
-ALTER TABLE "like" DROP CONSTRAINT like_post_owner_fkey1;
+ALTER TABLE "like" DROP CONSTRAINT IF EXISTS like_post_owner_post_uuid_fkey;
+ALTER TABLE "like" DROP CONSTRAINT IF EXISTS like_post_owner_fkey1;
 ALTER TABLE "like" ADD FOREIGN KEY (post_owner, post_uuid) REFERENCES post(owner, uuid) ON UPDATE CASCADE;
 
 COMMIT;
